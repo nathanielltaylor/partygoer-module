9e316689b6d71e194463791c1492273f2f6733e0
nathanielltaylor <ntaylor390@gmail.com>
2015-09-03 20:43:08 -0400

    initial

---
+++ code.rb
@@ -0,0 +1,52 @@
+module PartyGoer
+  #YOUR CODE GOES HERE
+
+
+  def drink
+    true
+  end
+
+  def add_drink
+    drink_count = 0
+    drink_count += 1
+    drink_count
+  end
+
+  def sing
+    "fvebvrtb"
+  end
+
+  def cause_havoc
+    raise PersonalizedHavocError, "You should define this yourself!"
+  end
+
+  def invited?
+    true
+  end
+
+end
+
+class PersonalizedHavocError < StandardError
+end
+
+
+class Launcher
+  include PartyGoer
+  extend PartyGoer
+
+  def initialize
+    @drink_count = 0
+  end
+
+  attr_accessor :drink_count
+
+  def drink
+    @drink_count += 1
+    if @drink_count < 3
+      super
+    else
+      false
+    end
+  end
+
+end
